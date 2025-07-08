9919728bacd0b548577e0e2fa1fbb21ddfe5c8ad
Your Name <you@example.com>
2025-07-08 12:43:40 +0300

    Вывод уроков в курсе

--- lms_backend/courses/serializers.py
+++ lms_backend/courses/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class LessonSerializer(serializers.ModelSerializer):
+class LessonShortSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = '__all__'
-
-
-def get_lessons_count(obj):
-    return obj.lessons.count()  # Получаем количество уроков
+        fields = ['id', 'title', 'description', 'preview', 'video_link']
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonShortSerializer(many=True, read_only=True, source='lessons_set')
     lessons_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = ['id', 'title', 'preview', 'description', 'lessons_count']
+        fields = ['id', 'title', 'preview', 'description', 'lessons_count', 'lessons']
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
