f4e7a810c67180e17c40c0073bbc2947cb1e7e41
Your Name <you@example.com>
2025-07-08 12:33:44 +0300

    Модель платежей

--- lms_backend/users/models.py
+++ lms_backend/users/models.py
@@ -12,3 +12,20 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payments')
+    payment_date = models.DateTimeField(auto_now_add=True)
+    paid_course = models.ForeignKey('courses.Course', on_delete=models.SET_NULL, null=True, blank=True)
+    paid_lesson = models.ForeignKey('courses.Lesson', on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES)
+
+    def __str__(self):
+        return f"Платеж {self.id} от {self.payment_date}"
