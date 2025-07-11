ffcaf929ecfacce3cbd3293b893edf2aff556442
Your Name <you@example.com>
2025-07-11 12:51:58 +0300

    merge

--- courses/models.py
+++ courses/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
-    preview = models.ImageField(upload_to='course_previews/', blank=True, null=True)
+    preview = models.ImageField(upload_to="course_previews/", blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
+
 class Lesson(models.Model):
-    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name="lessons", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True)
+    preview = models.ImageField(upload_to="lesson_previews/", blank=True, null=True)
     video_link = models.URLField(blank=True, null=True)
